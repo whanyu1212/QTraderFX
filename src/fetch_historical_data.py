282e6d9fcfc6bef668c7be088b7cffd760556b6a
Hanyu Wu <whanyu1212@hotmail.com>
2024-04-13 14:07:57 +0800

    refactoring

--- src/fetch_historical_data.py
+++ src/fetch_historical_data.py
@@ -1,7 +1,6 @@
 import oandapyV20
-import pandas as pd
-from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
+import pandas as pd
 
 
 class FetchHistoricalData:
@@ -14,10 +13,19 @@ class FetchHistoricalData:
         self.token = token
         self.timezone = timezone
         self.client = oandapyV20.API(access_token=self.token)
+        self.params = {"granularity": self.granularity, "count": self.count}
 
-    def fetch_data(self):
-        params = {"granularity": self.granularity, "count": self.count}
-        r = instruments.InstrumentsCandles(instrument=self.instrument, params=params)
+    def fetch_data(self) -> pd.DataFrame:
+        """
+        Fetch historical data from the OANDA API and process it into a
+        dataframe.
+
+        Returns:
+            pd.DataFrame: dataframe containing the historical data
+        """
+        r = instruments.InstrumentsCandles(
+            instrument=self.instrument, params=self.params
+        )
         self.client.request(r)
 
         data = [
@@ -34,26 +42,42 @@ class FetchHistoricalData:
         df = pd.DataFrame(data)
         return df
 
-    def process_data(self, df):
+    def check_columns(self, df: pd.DataFrame) -> None:
         expected_columns = ["Time", "High", "Close", "Low", "Open"]
-
         if not set(expected_columns).issubset(df.columns):
             raise ValueError(
-                f"DataFrame is missing one or more of the expected columns: {expected_columns}"
+                "DataFrame is missing one or more of ",
+                f"the expected columns: {expected_columns}",
             )
 
+    def convert_time(self, df: pd.DataFrame) -> pd.DataFrame:
         try:
             df["Time"] = pd.to_datetime(df["Time"]).dt.tz_convert(self.timezone)
+        except pd.errors.OutOfBoundsDatetime:
+            raise ValueError(
+                "Error converting 'Time' column to datetime: dates are out of bounds"
+            )
         except Exception as e:
             raise ValueError(f"Error converting 'Time' column to datetime: {e}")
+        return df
 
+    def set_index(self, df: pd.DataFrame) -> pd.DataFrame:
         df.set_index("Time", inplace=True)
+        return df
 
+    def convert_to_numeric(self, df: pd.DataFrame) -> pd.DataFrame:
+        expected_columns = ["High", "Close", "Low", "Open"]
         try:
-            df[expected_columns[1:]] = df[expected_columns[1:]].apply(pd.to_numeric)
+            df[expected_columns] = df[expected_columns].apply(pd.to_numeric)
         except Exception as e:
             raise ValueError(f"Error converting columns to numeric: {e}")
+        return df
 
+    def process_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        self.check_columns(df)
+        df = self.convert_time(df)
+        df = self.set_index(df)
+        df = self.convert_to_numeric(df)
         return df
 
     def flow(self):
